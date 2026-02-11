f05596a6c6b879bdafab424fcf2e1f649eaf51a3
leigit-dev <2578223561@qq.com>
2026-02-11 16:18:32 +0800

    "heartpump-test1"

---
+++ functions/heartpumb.js
@@ -0,0 +1,50 @@
+function read_connect(cncs){
+    let ans=cncs.split(";");
+    let anss={};
+    for (var i = 0; i <ans.length-1; i++) {
+        anss[ans[i]["id"]]=ans[i]["last-time"]
+    }
+    return anss;
+}
+
+function write_connect(cncs){
+    let ans="";
+    for (let k in cncs) {
+        ans=ans+k+":"+cncs[k]+";";
+        //anss[ans[i]["id"]]=ans[i]["last-time"]
+    }
+    return anss;
+}
+export async function onRequestGet(context) {
+    try {
+        const req = await context.request.json();
+        const id=req["id"];
+        const time=req["time"];
+
+        const st = await context.env.store.get('connection');
+        let stj=read_connect(st);
+
+        stj[id]=time;
+
+        let cns=write_connect(stj)
+        await context.env.store.put('connection', value);
+        //const value = await context.env.store.get('connection');
+        
+        // 返回响应
+        return new Response(JSON.stringify({
+            success: true,
+            value: stj
+        }), {
+            headers: { 'Content-Type': 'application/json' }
+        });
+    } 
+    catch (error) {
+        return new Response(JSON.stringify({
+            success: false,
+            error: error.message
+        }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+}
