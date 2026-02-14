03391667c93ea25ffdc126a6831b7d8c189c52e8
leigit-dev <2578223561@qq.com>
2026-02-14 20:47:58 +0800

    "ip6-25"

--- functions/heartpumb.js
+++ functions/heartpumb.js
@@ -6,7 +6,7 @@ export async function onRequestPost(context) {
         const resi = await context.env.command.get('receiver-situ');
         const [receiver,situ]=resi.split("|");
         
-        if(id==receiver||situ=="PENDING"){
+        if(id==receiver&&situ=="PENDING"){
             const cmd=await context.env.command.get('cmd');
             await context.env.command.put('receiver-situ', receiver+"|RECEIVED");
             return new Response(JSON.stringify({
